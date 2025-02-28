3cfd3cf9486aaf9e8679d58edfc2a29443ca2691
Ian VanSchooten <ian.vanschooten@gmail.com>
2025-02-28 15:51:59 -0500

    Update lit template

---
+++ v3/internal/templates/lit/frontend/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import wailsTypedEventsPlugin from "@wailsio/runtime/plugins/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [wailsTypedEventsPlugin("./bindings")],
+})
