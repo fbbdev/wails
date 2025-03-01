03d06f2c4be1df9763f95ff9980c9145660378e3
Fabio Massaioli <fabio.massaioli@gmail.com>
2025-03-01 13:09:12 +0100

    Explain build target config

--- v3/internal/templates/lit/frontend/vite.config.js
+++ v3/internal/templates/lit/frontend/vite.config.js
@@ -4,4 +4,10 @@ import WailsTypedEvents from "@wailsio/runtime/plugins/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [WailsTypedEvents("./bindings")],
+  build: {
+    // Wails supports deployment on macOS 10.13 High Sierra,
+    // whose webview is based on the same runtime as Safari 11.
+    // If you are targeting later macOS versions, you can change or remove this option.
+    target: "safari11"
+  }
 })
--- v3/internal/templates/vanilla/frontend/vite.config.js
+++ v3/internal/templates/vanilla/frontend/vite.config.js
@@ -5,6 +5,9 @@ import WailsTypedEvents from "@wailsio/runtime/plugins/vite";
 export default defineConfig({
   plugins: [WailsTypedEvents("./bindings")],
   build: {
+    // Wails supports deployment on macOS 10.13 High Sierra,
+    // whose webview is based on the same runtime as Safari 11.
+    // If you are targeting later macOS versions, you can change or remove this option.
     target: "safari11"
-  },
+  }
 });
