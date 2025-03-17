1e4ef69e9dca0cb674c4e6a8c1f3cb21e43fd014
Fabio Massaioli <fabio.massaioli@gmail.com>
2025-03-17 07:12:10 +0100

    Fix qwik-ts vite config

--- v3/internal/templates/qwik-ts/frontend/vite.config.ts
+++ v3/internal/templates/qwik-ts/frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
+import wails from "@wailsio/runtime/plugins/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,6 @@ export default defineConfig({
     qwikVite({
       csr: true,
     }),
+    wails("./bindings"),
   ],
 });
